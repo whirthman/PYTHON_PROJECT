bdbf27c1a780f38a9dfbe7d42e261445b82ce41a
whirthman <whirthman@gmail.com>
2023-11-01 17:14:35 +0000

    Escape character / sequence

--- firstPie.py
+++ firstPie.py
@@ -1,13 +1,3 @@
-# Formating Strings
-quantity = 3
-itemno = 567
-price = 49.95
-myorder = "I want {} pieces of item {} for ${}"
-
-print(myorder.format(quantity, itemno, price))
-
-text = "My name is {0}, Ive been in {1} for the past {2} years"
-name = input('What is your name')
-city = input('Which city are you living currently')
-years = input('How many years have reside in your current city')
-print(text.format(name,city,years))
+# Escape character / sequencer
+txt = "We are the so-called \"Vikings\" from the north. \nVikings and Scandinavians, well i dont know thier diffrence all i know is from the north"
+print(txt)
