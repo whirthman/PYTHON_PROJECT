01fe6a52d370edd1ba936a861a3faaae002d3b62
whirthman <whirthman@gmail.com>
2023-11-01 16:44:21 +0000

    Modify string

--- firstPie.py
+++ firstPie.py
@@ -1,5 +1,19 @@
 # String slicing in Python
+"""
 name = input("Enter your firs name ").upper()
 contact = input("Enter your contact: ")
 digit_slice = contact[0:5]
 print("Your name is ", name, " and your first 4 digit is ", digit_slice)
+"""
+
+# Remove white space
+a = " Hello, World! Hoe  "
+# print(len(a))
+# print(a.strip())
+# print(len(a))
+
+# Replace method
+print(a.replace("H","J"))
+
+# split method
+print(a.split(" "))
