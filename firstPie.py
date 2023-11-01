d1b5d5a2aa0e06b42b90aaaccf55654b3c86c23b
whirthman <whirthman@gmail.com>
2023-11-01 16:46:49 +0000

    Concantenate Strings

--- firstPie.py
+++ firstPie.py
@@ -1,19 +1,4 @@
-# String slicing in Python
-"""
-name = input("Enter your firs name ").upper()
-contact = input("Enter your contact: ")
-digit_slice = contact[0:5]
-print("Your name is ", name, " and your first 4 digit is ", digit_slice)
-"""
-
-# Remove white space
-a = " Hello, World! Hoe  "
-# print(len(a))
-# print(a.strip())
-# print(len(a))
-
-# Replace method
-print(a.replace("H","J"))
-
-# split method
-print(a.split(" "))
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
