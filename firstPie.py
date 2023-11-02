7da8c6fe32fadd31e280c958c0ebeee09de02dc2
whirthman <whirthman@gmail.com>
2023-11-02 18:07:56 +0000

    isspace method

--- firstPie.py
+++ firstPie.py
@@ -1,6 +1,6 @@
-# isPrintable method  
-# Returns true if all characters are printable
+# isSpace method  
+# Returns true if all characters are whitespace
 
-txt = "Hello! Are you #1?"
-x = txt.isprintable()
+txt = "     "
+x = txt.isspace()
 print(x)
