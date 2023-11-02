f835b8fe27dcbf3625e003ef0ddee34804802c48
whirthman <whirthman@gmail.com>
2023-11-02 16:08:25 +0000

    Casefold Method

--- firstPie.py
+++ firstPie.py
@@ -1,3 +1,5 @@
-# Capitalize Method
-txt = "python is fun coding!!!"
+# Casefold Method
+# Returns a string where all characters are lower case
+
+txt = "hello And Welcome to My World"
 print(txt.capitalize())
