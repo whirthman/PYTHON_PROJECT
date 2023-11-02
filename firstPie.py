7f329e5f063d14a4d9f982ee7a5abbec87ca70ad
whirthman <whirthman@gmail.com>
2023-11-02 17:04:19 +0000

    Encode and Decode Methods

--- firstPie.py
+++ firstPie.py
@@ -1,6 +1,14 @@
-# Count Method
-# Count number of times a character value appears in string
+# Encode Method
+# Encodes strings of characters 
 
-txt = "I love apples, apple are my favorite fruit"
-x = txt.count("apple")
-print(x)
+txt = "Whirthingham Wilson"
+x = txt.encode(encoding="ascii")
+print(txt.encode(encoding="UTF-8", errors="strict"))
+print(txt.encode(encoding="ISO-8859-1", errors="strict"))
+print(txt.encode(encoding="UTF-16",errors="xmlcharrefreplace"))
+print(txt.encode(encoding="UTF-32",errors="replace"))
+print(txt.encode(encoding="ascii",errors="backslashreplace"))
+
+store_encode = txt.encode(encoding="UTF-16",errors="xmlcharrefreplace")
+
+print(store_encode.decode("UTF-16"), " : This is a decoded string")
