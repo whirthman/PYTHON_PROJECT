f36e36f4193ff4b1e7135367a5523f5d87e7d2d1
whirthman <whirthman@gmail.com>
2023-11-02 17:17:00 +0000

    Endswith Methodd

--- firstPie.py
+++ firstPie.py
@@ -1,14 +1,6 @@
-# Encode Method
-# Encodes strings of characters 
+# Endswith  Method
+# Returns Boolean true if endswith specified chars 
 
 txt = "Whirthingham Wilson"
-x = txt.encode(encoding="ascii")
-print(txt.encode(encoding="UTF-8", errors="strict"))
-print(txt.encode(encoding="ISO-8859-1", errors="strict"))
-print(txt.encode(encoding="UTF-16",errors="xmlcharrefreplace"))
-print(txt.encode(encoding="UTF-32",errors="replace"))
-print(txt.encode(encoding="ascii",errors="backslashreplace"))
-
-store_encode = txt.encode(encoding="UTF-16",errors="xmlcharrefreplace")
-
-print(store_encode.decode("UTF-16"), " : This is a decoded string")
+x = txt.endswith("Son")
+print(x)
