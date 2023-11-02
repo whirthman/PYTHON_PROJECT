95b368c7e68cbb60c0072886bf0c4ae0853d7a5c
whirthman <whirthman@gmail.com>
2023-11-02 16:05:10 +0000

    Capitalize Method

--- firstPie.py
+++ firstPie.py
@@ -1,17 +1,3 @@
-#String Methods
-
-# Enocode
-# txt = "My name is Ståle"
-
-# print(txt.encode(encoding="ascii",errors="backslashreplace"))
-# print(txt.encode(encoding="ascii",errors="ignore"))
-# print(txt.encode(encoding="ascii",errors="namereplace"))
-# print(txt.encode(encoding="ascii",errors="replace"))
-# print(txt.encode(encoding="ascii",errors="xmlcharrefreplace"))
-
-# Center
-txt = "banana"
-
-x = txt.center(30, "O")
-
-print(x)
+# Capitalize Method
+txt = "python is fun coding!!!"
+print(txt.capitalize())
