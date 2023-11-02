e07b5a2357899d51b6b56d2dbfbaa77ee4b88787
whirthman <whirthman@gmail.com>
2023-11-02 15:57:33 +0000

    Python String Methods

--- firstPie.py
+++ firstPie.py
@@ -1,3 +1,17 @@
-# Escape character / sequencer
-txt = "We are the so-called \"Vikings\" from the north. \nVikings and Scandinavians, well i dont know thier diffrence all i know is from the north"
-print(txt)
+#String Methods
+
+# Enocode
+# txt = "My name is Ståle"
+
+# print(txt.encode(encoding="ascii",errors="backslashreplace"))
+# print(txt.encode(encoding="ascii",errors="ignore"))
+# print(txt.encode(encoding="ascii",errors="namereplace"))
+# print(txt.encode(encoding="ascii",errors="replace"))
+# print(txt.encode(encoding="ascii",errors="xmlcharrefreplace"))
+
+# Center
+txt = "banana"
+
+x = txt.center(30, "O")
+
+print(x)
