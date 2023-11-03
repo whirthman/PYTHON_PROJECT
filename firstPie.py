2129dab6a1c9081753cf4c8373c18d7b3fcbdbd6
whirthman <whirthman@gmail.com>
2023-11-03 14:41:11 +0000

    Join method

--- firstPie.py
+++ firstPie.py
@@ -1,5 +1,6 @@
-# isUpper method  
-# Returns true if chars are upper case
-txt = "WELCOME"
-x = txt.isupper()
+# join method  
+# Joins list or tuples into a string using string seperator
+myDict = ("John", "Peter", "Vicky")
+seperator = "..."
+x = seperator.join(myDict)
 print(x)
