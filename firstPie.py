c7fefb86c7afcad14bb1f9f5ae7656deee91222b
whirthman <whirthman@gmail.com>
2023-11-04 14:48:06 +0000

    python operators

--- firstPie.py
+++ firstPie.py
@@ -1,15 +1,9 @@
-# Every values are true with an exceptions
+# Python Operators
 
-# Any string is True, except empty strings.
-
-# Any number is True, except 0.
-
-# Any list, tuple, set, and dictionary are True, except empty ones.
-
-print(bool("abc"))
-print(bool(123))
-print(bool(["apple", "cherry", "banana"]))
-print(bool(0))
-print(bool(""))
-print(bool([]))
-
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
