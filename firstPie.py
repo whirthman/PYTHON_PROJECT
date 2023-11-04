052116228c10ffcb53ff29a70584b87c70616445
whirthman <whirthman@gmail.com>
2023-11-04 16:19:17 +0000

    Arthmetric Operators

--- firstPie.py
+++ firstPie.py
@@ -1,9 +1,24 @@
-# Python Operators
+#  Arthmetric Operators
+x = 2
+y = 3
 
-# Arithmetic operators
-# Assignment operators
-# Comparison operators
-# Logical operators
-# Identity operators
-# Membership operators
-# Bitwise operators
+# Additon
+print("Additon: " + str(x)+"+"+str(y) + "=", x+y)
+
+# Substraction
+print("Substraction: " + str(x)+"-"+str(y) + "=", x-y)
+
+# Multiplication
+print("Multiplication: " + str(x)+"*"+ str(y) + "=", x*y)
+
+# Division
+print("Division: " + str(x) + "/" + str(y) + "=", x/y )
+
+# Modulus
+print("Modulus :" + str(x) + "%" + str(y) + "=", x%y)
+
+# Exponentiation
+print("Exponentiation :" + str(x) + "**" + str(y) + "=", x**y)
+
+# Floor Division
+print("Floor Division: " + str(x) + "//" + str(y) + "=", x//y )
