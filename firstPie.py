9cfc726643a54fd6a627026d89cb78617281168e
whirthman <whirthman@gmail.com>
2023-11-04 14:02:19 +0000

    running a condito statement using booleans

--- firstPie.py
+++ firstPie.py
@@ -1,5 +1,8 @@
-# Booleans
+# When you run a condition in an if statement, Python returns True or False:
+a = 200
+b = 33
 
-print(10 > 9)
-print(10 == 9)
-print(10 < 9)
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
