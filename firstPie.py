d5c994eab857ea340b6d0e6c4389a1b637b0a1b6
whirthman <whirthman@gmail.com>
2023-11-04 13:39:33 +0000

    Random Numbers in Python

--- firstPie.py
+++ firstPie.py
@@ -1,18 +1,7 @@
 # Numbers in Python
 
-# Type Conversion
+# Random Numbers in pythond
 
-x = 1    # int
-y = 2.8  # float
-# z = 1j   # complex
+import random
 
-# a = int(z)
-b = float(x)
-c = complex(y)
-
-# print(a)
-# print("Type of a: ", type(a))
-print(b)
-print("Type of b: ", type(b))
-print(c)
-print("Type of ", type(c))
+print(random.randrange(0,10))
