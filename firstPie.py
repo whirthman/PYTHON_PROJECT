a7f2ec678cd09f0a8271ca2e1e13964408b028ef
whirthman <whirthman@gmail.com>
2023-11-04 22:01:58 +0000

    Assignment Operators

--- firstPie.py
+++ firstPie.py
@@ -1,33 +1,14 @@
 #  Arthmetric Operators Program
 
-x = int(input("Enter the first value (x)"))
-y = int(input("Enter the second value (y)"))
-z = input("Input your operator")
-
-# Additon
-if(z == "+"):
-    print("Additon: " + str(x)+"+"+str(y) + "=", x+y)
-
-# Substraction
-elif(z == "-"):
-    print("Substraction: " + str(x)+"-"+str(y) + "=", x-y)
-
-# Multiplication
-elif(z == "*"):
-    print("Multiplication: " + str(x)+"*"+ str(y) + "=", x*y)
-
-# Division
-elif(z == "/"):
-    print("Division: " + str(x) + "/" + str(y) + "=", x/y )
-
-# Modulus
-elif(z == "%"):
-    print("Modulus :" + str(x) + "%" + str(y) + "=", x%y)
-
-# Exponentiation
-elif(z == "**"):
-    print("Exponentiation :" + str(x) + "**" + str(y) + "=", x**y)
-
-# Floor Division
-elif(z == "//"):
-    print("Floor Division: " + str(x) + "//" + str(y) + "=", x//y )
+x = 5
+x += 3
+x -= 3
+x *=3
+x //= 3
+x **= 3
+x &= 3
+x %= 3
+x ^= 3
+x >>= 5
+x <<= 100
+print(x)
