037f037c1e57b381096e6fc719dc74c5d353eb77
whirthman <whirthman@gmail.com>
2023-11-05 16:25:02 +0000

    Python Logical Operators

--- firstPie.py
+++ firstPie.py
@@ -1,11 +1,8 @@
-#  Python Comparison Operators
+#  Python Logical Operators
 
-x = 5
-y = 3
+x = 3
+y = 5
 
-print(x==y)
-print(x!=y)
-print(x>y)
-print(x<y)
-print(x>=y)
-print(x<=y)
+print (	x < 5 and  x < 10)
+print(x < 5 or x < 4)
+print(not(x < 5 and x < 10))
