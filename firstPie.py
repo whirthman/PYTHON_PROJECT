ecb8b3fbb160e47f47cfc0f2a976de8fc3019d5c
whirthman <whirthman@gmail.com>
2023-11-05 17:09:50 +0000

    Lenght, Type and Creatin List Constructor Method

--- firstPie.py
+++ firstPie.py
@@ -1,3 +1,13 @@
 # LIST
 myList = ["Banana", "Cherry", "Apple"]
 print(myList)
+
+# Finding the Lenght
+print(len(myList))
+
+# Chceking the type
+print(type(myList))
+
+# Creatin list with constructor
+thisItems = list(("John", "Jonah", "Jeremiah"))
+print(thisItems)
