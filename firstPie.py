c7fddd5a431b1db505ef7fde6ea390dd5344bf32
whirthman <whirthman@gmail.com>
2023-11-05 20:54:04 +0000

    List Comprehension

--- firstPie.py
+++ firstPie.py
@@ -2,22 +2,31 @@
 west_africa = ["Ghana","Nigeria","Ivory Coast","Togo","Senegal","Burkina Faso","Liberia"]
 east_africa = ["Kenya", "Ethopia", "Uganda", "Tanzania"]
 
-# Using the For Loop
+# # Using the For Loop
 
+# for country in west_africa:
+#     print(country)
+
+# for i in range(len(west_africa)):
+#     print(west_africa[i])
+
+
+# #  Using the While Loop
+# i = 0
+# while i < len(east_africa):
+#     print(east_africa[i])
+#     i += 1
+
+# print(range(len(east_africa)))
+
+# # Looping using Comprehension
+# [print(x) for x in east_africa]
+
+
+# List Comprehension
+new_country = []
 for country in west_africa:
-    print(country)
+    if("G" in country):
+        new_country.append(country)
 
-for i in range(len(west_africa)):
-    print(west_africa[i])
-
-
-#  Using the While Loop
-i = 0
-while i < len(east_africa):
-    print(east_africa[i])
-    i += 1
-
-print(range(len(east_africa)))
-
-# Looping using Comprehension
-[print(x) for x in east_africa]
+print(new_country)
