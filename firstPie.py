b1438dc908bd847ed7b425a6c49254ffa7f9b2b0
whirthman <whirthman@gmail.com>
2023-11-05 16:33:26 +0000

    Python Identity Operators

--- firstPie.py
+++ firstPie.py
@@ -1,8 +1,8 @@
-#  Python Logical Operators
+#  Python Identity Operators
 
 x = 3
 y = 5
 
 print (	x < 5 and  x < 10)
 print(x < 5 or x < 4)
-print(not(x < 5 and x < 10))
+print(not(x < 5 and x < 10))git 
