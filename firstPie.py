9fc01929ad7cd58522d5d7633cb557754dcb70dd
whirthman <whirthman@gmail.com>
2023-11-08 13:30:28 +0000

    Joining List in Python

--- firstPie.py
+++ firstPie.py
@@ -1,4 +1,18 @@
-#  Copy
-fruits = ["Banana", "Apple", "Mangoes", "Pineapple"]
-fruits2 = fruits.copy()
-print(fruits2)
+# Joining List
+list1 = [1,2,3,4,5,6,7]
+list2 = ["a","b","c"]
+
+list3 = list1 + list2
+print(list3)
+
+west_africa = ["Nigeria", "Burkina Faso", "Ghana","Togo","Senegal","Mali","Cote Divoire","Liberai"]
+east_africa = ["Tanzania","Kenya","South Sudan","Ethiopia","Uganda","Somalia"]
+
+africa = []
+africa.extend(east_africa)
+print(africa)
+
+for x in west_africa:
+    africa.append(x)
+
+print(africa)
