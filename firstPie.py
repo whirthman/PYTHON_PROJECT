e0e19fa53aae99b3a5a169db8933ca63242e34fa
whirthman <whirthman@gmail.com>
2023-11-08 20:16:45 +0000

    Accesing Indexes

--- firstPie.py
+++ firstPie.py
@@ -1,3 +1,22 @@
 # Tuple
 west_africa = ("Ghana","Nigeria","Cote Divoier","Togo","Senegal")
 print(west_africa)
+
+# Tuple Length
+print(len(west_africa))
+
+# Cheking the Type
+print(type(west_africa))
+
+# Creating Tuple form Constructor
+east_africa = tuple(("Kenya","Uganda","Ethopia","Tanzania"))
+print(east_africa)
+
+# Accesing the Tuple
+print(west_africa[0])
+
+# Negative Indexing
+print(west_africa[-1])
+
+# Range of Index
+print(west_africa[2:4])
