1c393e8c8848309f60867dd2beb9d37fa6c98047
whirthman <whirthman@gmail.com>
2023-11-08 20:10:31 +0000

    Creating a Tuple

--- firstPie.py
+++ firstPie.py
@@ -1,18 +1,3 @@
-# Joining List
-list1 = [1,2,3,4,5,6,7]
-list2 = ["a","b","c"]
-
-list3 = list1 + list2
-print(list3)
-
-west_africa = ["Nigeria", "Burkina Faso", "Ghana","Togo","Senegal","Mali","Cote Divoire","Liberai"]
-east_africa = ["Tanzania","Kenya","South Sudan","Ethiopia","Uganda","Somalia"]
-
-africa = []
-africa.extend(east_africa)
-print(africa)
-
-for x in west_africa:
-    africa.append(x)
-
-print(africa)
+# Tuple
+west_africa = ("Ghana","Nigeria","Cote Divoier","Togo","Senegal")
+print(west_africa)
