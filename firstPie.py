1d4b622e6b100a2e876e8dcbb2299aac40db01d8
whirthman <whirthman@gmail.com>
2023-11-09 21:50:31 +0000

    Adding Items to Sets

--- firstPie.py
+++ firstPie.py
@@ -5,3 +5,12 @@ print(countries)
 # Accesing a Sets by Looping
 for country in countries:
     print(country)
+
+# Adding items to Sets
+countries.add("United States")
+print(countries)
+
+# Adding an Iterable to a Set
+eu_count = ["Spain", "Portugal", "France"]
+countries.update(eu_count)
+print(countries)
