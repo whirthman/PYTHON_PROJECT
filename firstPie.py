e3af1a2b7dc622a1625a6ad83676763ed1976dbd
whirthman <whirthman@gmail.com>
2023-11-09 22:33:07 +0000

    Creating a Dictionary Objects

--- firstPie.py
+++ firstPie.py
@@ -1,56 +1,22 @@
-# Set
-countries = {"Malta", "Ghana", "Italy", "United Kingdom"}
-print(countries)
+# Dictionary
+user = {
+    "name" : "Jonah",
+    "age" :   "23",
+    "proffesion" : "Developer"
+}
 
-# Accesing a Sets by Looping
-for country in countries:
-    print(country)
+print(user)
 
-# Adding items to Sets
-countries.add("United States")
-print(countries)
+# Accesing Items in list
+print(user["name"])
 
-# Adding an Iterable to a Set
-eu_count = ["Spain", "Portugal", "France"]
-countries.update(eu_count)
-print(countries)
+# Length of Dictionary
+print(len(user))
 
-# Removing Items from Sets
-countries.remove("Italy")
-print(countries)
 
-# Using the Discard Method
-countries.discard("France")
-print(countries)
+# Finding  Type of Dictionary Object
+print(type(user))
 
-# Looping throug a Sets
-for country in countries:
-    print(country)
-
-africa = {"Nigeria", "Kenya","Uganda","Senegal","Ghana"}
-nations = africa.union(countries)
-print(nations)
-
-# Using the Update Method
-middle_east = {"Turkey", "Israel", "Saudi Arabia", "UAE"}
-nations.update(middle_east)
-print(nations)
-
-# Keeping only the Duplicate / Intersectio of Sets
-fruits = {"apple", "mango", "banana"}
-tech = {"microsoft", "google", "apple"}
-intersect = fruits.intersection(tech)
-print(intersect)
-
-# Keeping both but not duplicate / using symetric_diffrecne method
-sym_diff = fruits.symmetric_difference(tech)
-print(sym_diff)
-
-# Findig only Sets Members with diffrence
-# Fruit Only
-diff_fruits = fruits.difference(tech)
-print(diff_fruits)
-
-# Tech Only
-diff_tech = tech.difference(fruits)
-print(diff_tech)
+# Creating Dictionary with Constructor
+user2 = dict(name = "John", age = 23, profession = "Designer")
+print(user2)
