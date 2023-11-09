25ebd08e654de85e734a4866b4ccb9901b808151
whirthman <whirthman@gmail.com>
2023-11-09 21:44:53 +0000

    Accesing a Sets by Looping

--- firstPie.py
+++ firstPie.py
@@ -2,16 +2,6 @@
 countries = {"Malta", "Ghana", "Italy", "United Kingdom"}
 print(countries)
 
-# True and 1 considered same value
-numb = {True, 4, 5, 12, 1, 0, True,}
-print(numb)
-
-# Lenght of Set
-print(len(countries))
-
-# Type of set
-print(type(countries))
-
-#  Creating sets with Constructor
-fruits = set({"Apple", "Banana", "Mango"})
-print(fruits)
+# Accesing a Sets by Looping
+for country in countries:
+    print(country)
