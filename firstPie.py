523d366589a3ac52d5622f4e71106d223adfab15
whirthman <whirthman@gmail.com>
2023-11-09 22:47:13 +0000

    Accesing Items

--- firstPie.py
+++ firstPie.py
@@ -20,3 +20,27 @@ print(type(user))
 # Creating Dictionary with Constructor
 user2 = dict(name = "John", age = 23, profession = "Designer")
 print(user2)
+
+# Accesing Dict Items
+print(user["proffesion"])
+
+# Getting Keys from Dict Object
+print(user.keys())
+
+print(user2.keys())
+
+print(user)
+
+user["hometown"] = "Goaso"
+
+print(user)
+
+# Using the Get Values Method
+print(user.values())
+
+# Get Items Method return as tuples
+print(user.items())
+
+# Checking if Key Exist
+if "hometown" in user:
+    print("He is ",user["name"], "and he comes from ", user["hometown"] )
